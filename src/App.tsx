02d52926af84d84b75fb6dcfb0127b52001b7491
Mateus Conde <devmateuscondefeitosa@gmail.com>
2022-09-14 13:31:30 -0300

    esqueleton paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Home } from "./pages/home";
+import { Login } from "./pages/login";
+import { Navbar } from "./components/navbar";
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}> </Route>
+          <Route path="/login" element={<Login />}> </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
