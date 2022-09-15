2de0009158289a34663fa4c7a38e0cbe971725bc
Mateus Conde <devmateuscondefeitosa@gmail.com>
2022-09-15 15:01:08 -0300

    Login css

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -17,21 +17,29 @@ export const Login = () => {
     }
 
     return (
-        <Container fluid="md" className="mt-5">
-            <Row g="0"> 
-                <Col lg="5">
-                    <img
-                    alt=""
-                    className="img-fluid" 
-                    src="https://images.unsplash.com/photo-1640537702474-3e503c21eefc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" 
-                    />
-                </Col>
-                <Col lg="7">
-                    <h1>Login</h1>
-                    <h3>Sign In With Google To Continue</h3>
-                    <button onClick={signInWithGoogle}>Sign In With Google</button>
-                </Col>
-            </Row>
-        </Container>
+        <section className="py-auto mt-5 login px-5 mx-5">
+            <Container fluid="sm">
+                <Row g="0"> 
+                    <Col lg="5" className="px-0">
+                        <img
+                        alt=""
+                        className="img-fluid" 
+                        src="https://images.unsplash.com/photo-1640537702474-3e503c21eefc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" 
+                        />
+                    </Col>
+                    <Col lg="7" className="text-center py-3">
+                        <div>
+                            <h1>Log In</h1> 
+                        </div>
+                        <div className="text-center py-3 pt-5">
+                            <h4>Sign In With Google To Continue</h4>  
+                        </div>
+                        <div className="offset-2 col-lg-8">
+                            <button onClick={signInWithGoogle} className="btnLogin">Google Log-In</button>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
     );
 } 
