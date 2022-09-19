ea7c003a7512a6d6cf20d6244145b3c679e632d6
Mateus Conde <devmateuscondefeitosa@gmail.com>
2022-09-19 16:35:43 -0300

    bootstrap cards

--- src/pages/main/post.tsx
+++ src/pages/main/post.tsx
@@ -1,24 +1,31 @@
-import { Posts } from './home';
-import Card from 'react-bootstrap/Card';
+import { Posts } from "./home";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
 interface Props {
-    post: Posts;
+  post: Posts;
 }
-
 
 export const Post = (props: Props) => {
-    const { post } = props;
-    return (
-    <Card style={{ width: '18rem' }}>
-        <Card.Body>
-            <Card.Title>{post.title}</Card.Title>
-            <Card.Text>{post.description}</Card.Text>
-            <blockquote className="blockquote mb-0">
-                <footer className="blockquote-footer">
-                    @{post.username}
-                </footer>
-            </blockquote>
-        </Card.Body>
+  const { post } = props;
+  return (
+    <Card border="info" style={{ width: "22rem" }} className="mx-4 my-4">
+      <Card.Body className="text-start">
+        <Card.Title>{post.title}</Card.Title>
+        <Card.Text>{post.description}</Card.Text>
+        <Button variant="light" size="sm">
+          {" "}
+          Like &#10084;{" "}
+        </Button>
+        <blockquote className="blockquote mb-0 d-inline text-start">
+          <footer
+            className="blockquote-footer text-end"
+            style={{ fontSize: "12px" }}
+          >
+            @{post.username}
+          </footer>
+        </blockquote>
+      </Card.Body>
     </Card>
-    )
-}
+  );
+};
